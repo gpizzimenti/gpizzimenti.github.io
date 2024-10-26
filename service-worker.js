ec57e761cc1730d2ea3c5c2e125d6cf22a23ab4c
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2024-10-26 15:30:07 +0200

    addressCountri in ISO 3166-1 alpha-2 format

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202410261520;
+const SW_VERSION = 202410261530;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
