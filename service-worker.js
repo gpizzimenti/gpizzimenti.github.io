671a06a604c468e6acf82edf03c86e1c353a73c7
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2025-03-15 12:07:46 +0100

    update calendar

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202412200950;
+const SW_VERSION = 2025011200;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
