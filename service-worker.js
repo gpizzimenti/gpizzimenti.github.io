48bf2ded972ce1602beffc5864fb804302504a7a
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2025-03-18 11:25:04 +0100

    link pagespeed ; default form_factor=desktop

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202503181116;
+const SW_VERSION = 202503181125;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
