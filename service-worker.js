b8a2a197b8b33e8922fd3d1d447f78f39b6edf98
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2025-10-23 13:15:14 +0200

    link preconnect

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202510231305;
+const SW_VERSION = 202510231315;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
