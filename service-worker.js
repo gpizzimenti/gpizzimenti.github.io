071fdc93ee194920769ee2b800d05a68251a644e
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2025-10-26 11:11:48 +0100

    added missing crossorigin attibute

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202510261110;
+const SW_VERSION = 202510261115;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
