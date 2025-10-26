b77b03abaa764d21b3c6ce24a18f9cebc85e84c3
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2025-10-26 11:15:52 +0100

    polyfill correct path :(

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202510261115;
+const SW_VERSION = 202510261120;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
