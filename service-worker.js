cae7da6378bc3e9de8f7ff8c11fc560c682111ba
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2026-02-15 12:19:48 +0100

    mod schema.json

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202602142010;
+const SW_VERSION = 202602151220;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
