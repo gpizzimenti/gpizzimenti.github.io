8582d5afb48355e000445c761a13be09b59348ae
Giuseppe Pizzimenti <g.pizzimenti@glauco.it>
2026-06-27 10:40:08 +0200

    update events & courses

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ import {
 } from './sw-assets.min.js';
 
 // service worker version number
-const SW_VERSION = 202602151220;
+const SW_VERSION = 202606271035;
 
 // cache name including version number
 const cacheName = `gpizzimenti-app-cache-${SW_VERSION}`;
